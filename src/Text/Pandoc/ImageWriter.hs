6b1b9c546ff20052e4c7d01f4ce89a20492edd9e
mpickering <matthewtpickering@gmail.com>
2014-05-11 00:12:00 +0100

    Added filter which writes encoded images to file so that they can be displayed in more formats

---
+++ src/Text/Pandoc/ImageWriter.hs
@@ -0,0 +1,36 @@
+{-
+
+Copyright
+
+Writes emdedded images to file and returns a modified AST with the new local paths.
+
+-}
+
+
+module Text.Pandoc.ImageWriter (convertImages) where
+
+import Text.Pandoc.Definition
+import qualified Data.ByteString as BS
+import qualified Text.Pandoc.UTF8 as UTF8
+import Data.Maybe (fromJust)
+import System.FilePath
+import Text.Pandoc.MIME (extensionFromMimeType)
+import Text.Pandoc.Generic (bottomUpM)
+
+
+convertB64 :: Inline -> IO Inline
+convertB64 (Image alt (Encoded (mime, b64))) = do
+  let tmpdir = "images"
+  let bs  = unByteString64 b64
+  let tit = UTF8.toString $ BS.take 10 bs
+  let ext = fromJust $ extensionFromMimeType mime
+  let fname = tmpdir </> tit <.> ext
+  BS.writeFile fname bs
+  return $ Image alt (Relative (fname, tit))
+convertB64 x = return x
+
+convertImages :: Pandoc -> IO Pandoc
+convertImages = bottomUpM convertB64
+
+
+  
