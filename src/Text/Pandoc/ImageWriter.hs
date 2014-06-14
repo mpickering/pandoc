a204b2f0648dc26b2c5e1fa45194164793ccf459
mpickering <matthewtpickering@gmail.com>
2014-06-14 16:16:17 +0100

    ImageWriter works

--- src/Text/Pandoc/ImageWriter.hs
+++ src/Text/Pandoc/ImageWriter.hs
@@ -11,26 +11,32 @@ module Text.Pandoc.ImageWriter (convertImages) where
 
 import Text.Pandoc.Definition
 import qualified Data.ByteString as BS
-import qualified Text.Pandoc.UTF8 as UTF8
+import qualified Data.ByteString.Char8 as B8
+import qualified Data.ByteString.Base64 as B64
 import Data.Maybe (fromJust)
 import System.FilePath
 import Text.Pandoc.MIME (extensionFromMimeType)
 import Text.Pandoc.Generic (bottomUpM)
+import Crypto.Hash.SHA1 (hash)
+import System.Directory (doesFileExist)
+import Control.Monad (unless)
 
 
 convertB64 :: Inline -> IO Inline
 convertB64 (Image alt title (ImageData mime b64)) = do
   let tmpdir = "images"
-  let bs  = unByteString64 b64
-  let tit = UTF8.toString $ BS.take 10 bs
+  let bs  = either error id  (B64.decode $ unByteString64 b64)
+  let tit = B8.unpack $ B64.encode $ hash bs
   let ext = fromJust $ extensionFromMimeType mime
   let fname = tmpdir </> tit <.> ext
-  BS.writeFile fname bs
+  exists <- doesFileExist fname
+  unless exists $ BS.writeFile fname bs
   return $ Image alt title (ImagePath fname)
 convertB64 x = return x
 
 convertImages :: Pandoc -> IO Pandoc
 convertImages = bottomUpM convertB64
+
 
 
 
