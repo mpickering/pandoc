34f79c97ebf5e477628d38981c0edca919b3d942
mpickering <matthewtpickering@gmail.com>
2014-06-18 19:42:12 +0100

    Added epub_exts option

--- src/Text/Pandoc/Options.hs
+++ src/Text/Pandoc/Options.hs
@@ -102,6 +102,7 @@ data Extension =
     | Ext_mmd_header_identifiers -- ^ Multimarkdown style header identifiers [myid]
     | Ext_implicit_header_references -- ^ Implicit reference links for headers
     | Ext_line_blocks         -- ^ RST style line blocks
+    | Ext_epub_html_exts      -- ^ Recognise the EPUB extended version of HTML
     deriving (Show, Read, Enum, Eq, Ord, Bounded)
 
 pandocExtensions :: Set Extension
@@ -142,6 +143,7 @@ pandocExtensions = Set.fromList
   , Ext_header_attributes
   , Ext_implicit_header_references
   , Ext_line_blocks
+  , Ext_epub_html_exts
   ]
 
 phpMarkdownExtraExtensions :: Set Extension
