02bb19ba5e7c61e0a5af087b0450d817f0a6a47f
Matthew Pickering <matthewtpickering@gmail.com>
2014-07-13 22:54:27 +0100

    Finalised implementation of core markup

--- src/Text/Pandoc/Readers/Txt2Tags.hs
+++ src/Text/Pandoc/Readers/Txt2Tags.hs
@@ -1,50 +1,33 @@
 {-# LANGUAGE ViewPatterns #-}
 
-module Text.Pandoc.Readers.Txt2Tags where
+module Text.Pandoc.Readers.Txt2Tags (readTxt2Tags) where
 
 import qualified Text.Pandoc.Builder as B
-import           Text.Pandoc.Builder ( Inlines, Blocks, HasMeta(..), (<>)
-                                     , trimInlines )
-import           Text.Pandoc.Definition
-import           Text.Pandoc.Options
-import           Text.Pandoc.Readers.LaTeX (inlineCommand, rawLaTeXInline)
-import           Text.Pandoc.Shared (splitBy, escapeURI,compactify', compactify'DL)
-import Text.Pandoc.Parsing hiding (space)
-import           Control.Applicative (  Applicative, pure
-                                     , (<$>), (<$), (<*>), (<*), (*>), (<**>) )
-import           Control.Monad (foldM, guard, liftM, liftM2, mplus, mzero, when, void)
-import           Data.Char (isAlphaNum, toLower)
-import           Data.Default
-import           Data.List (transpose, intersperse, isPrefixOf, isSuffixOf, intercalate)
-import qualified Data.Map as M
-import           Data.Maybe (fromMaybe, isJust, maybe)
-import           Data.Monoid (Monoid, mconcat, mempty, mappend)
-import           Network.HTTP (urlEncode)
-import Debug.Trace
-import Data.Sequence (viewr, ViewR(..))
-import Network.URI (isURI)
-import Control.Monad.Reader
-import Text.Parsec.Prim hiding (many)
-import Text.Parsec (ParseError)
-
-validChars :: String
-validChars = ['a' .. 'z'] ++ ['A' .. 'Z'] ++ ['0' .. '9'] ++ "_-"
+import Text.Pandoc.Builder ( Inlines, Blocks, (<>)
+                           , trimInlines )
+import Text.Pandoc.Definition
+import Text.Pandoc.Options
+import Text.Pandoc.Shared (escapeURI,compactify', compactify'DL)
+import Text.Pandoc.Parsing hiding (space, spaces, uri)
+import Control.Applicative ((<$>), (<$), (<*>), (<*), (*>))
+import Data.Char (toLower)
+import Data.List (transpose, intersperse, intercalate)
+import Data.Maybe (fromMaybe)
+import Data.Monoid (Monoid, mconcat, mempty, mappend)
+--import Network.URI (isURI) -- Not sure whether to use this function
+import Control.Monad (void, guard, mzero, when)
 
 type T2T = Parser String ParserState 
 
-
-parseTxt2Tags :: ReaderOptions -> String -> Pandoc
-parseTxt2Tags opts s = either (error . show) id (runT2T parseT2T (def {stateOptions = opts}) s)
-
-runT2T :: T2T a -> ParserState -> String ->  Either ParseError a
-runT2T t s inp = runParser t s "" inp
-
+readTxt2Tags :: ReaderOptions -> String -> Pandoc
+readTxt2Tags opts s = readWith parseT2T (def {stateOptions = opts}) (s ++ "\n\n")
 
 parseT2T :: T2T Pandoc
 parseT2T = do
-  meta <- (Nothing <$ blankline) <|> (Just <$> (count 3 anyLine))
+  _ <- (Nothing <$ blankline) <|> (Just <$> (count 3 anyLine))
   config <- manyTill setting (notFollowedBy setting)
-  let meta = foldr (\(k,v) -> B.setMeta k v) nullMeta config
+  let settings = foldr (\(k,v) -> B.setMeta k (MetaString v)) nullMeta config
+  updateState (\s -> s {stateMeta = settings})
   body <- mconcat <$>  manyTill block eof
   return $ Pandoc mempty (B.toList body)
 
@@ -68,13 +51,13 @@ block :: T2T Blocks
 block = do
   choice 
     [ mempty <$ blanklines
+    , quote  
+    , hrule -- hrule must go above title
     , title
-    , comment
-    , hrule
+    , commentBlock
     , verbatim
     , rawBlock
     , taggedBlock
-    , quote  
     , list
     , table
     , para
@@ -85,40 +68,38 @@ title = try $ balancedTitle '+' <|> balancedTitle '='
 
 balancedTitle :: Char -> T2T Blocks
 balancedTitle c = try $ do
+  spaces
   level <- length <$> many1 (char c) 
   guard (level <= 5) -- Max header level 5
-  heading <- manyTill anyChar (lookAhead $ char c)
-  count level (char c)
-  label <- optionMaybe (enclosed (char '[') (char ']') (oneOf validChars))
-  spaces *> newline
+  heading <- manyTill (noneOf "\n\r") (count level (char c))
+  label <- optionMaybe (enclosed (char '[') (char ']') (alphaNum <|> oneOf "_-"))
+  many spaceChar *> newline
   let attr = maybe nullAttr (\x -> (x, [], [])) label
-  return $ B.headerWith attr level (B.text heading)
+  return $ B.headerWith attr level (trimInlines $ B.text heading)
 
 para :: T2T Blocks
-para = try (B.para <$> trimInlines . mconcat <$> many1Till inline newline)
+para = try (B.para <$> parseInlines)
 
 commentBlock :: T2T Blocks 
-commentBlock = try (blockMarkupArea (anyLine) (const mempty) "%%%")
+commentBlock = try (blockMarkupArea (anyLine) (const mempty) "%%%") <|> comment
 
 -- Seperator and Strong line treated the same
 hrule :: T2T Blocks 
 hrule = try $ do
   spaces 
-  line <- manyTill (oneOf "=-_") (newline <|> space)
+  line <- many1 (oneOf "=-_") 
   guard (length line >= 20)
   B.horizontalRule <$ blankline 
-  
   
 quote :: T2T Blocks
 quote = try $ do
   lookAhead tab
-  raw <-  many1 (tab *> optional spaces *> anyLine)
-  contents <- parseFromString (parseBlocks) (intercalate "\n" raw ++ "\n\n")
+  rawQuote <-  many1 (tab *> optional spaces *> anyLine)
+  contents <- parseFromString parseBlocks (intercalate "\n" rawQuote ++ "\n\n")
   return $ B.blockQuote contents 
 
-comment :: T2T Blocks
-comment = try $ (mempty <$ (char '%' *> anyLine)) <|> commentBlock
-
+commentLine :: T2T Inlines
+commentLine = comment
 
 -- List Parsing code from Org Reader
 
@@ -141,7 +122,7 @@ definitionList = do
 toDefinitionList :: Blocks -> T2T (Inlines, [Blocks]) 
 toDefinitionList (B.toList -> (Para is :bs)) = 
   return (B.fromList is, [B.fromList bs])
-toDefinitionList b = mzero
+toDefinitionList _ = mzero
 
 genericListStart :: T2T Char
                  -> T2T Int
@@ -190,7 +171,7 @@ indentWith n = count n space
 table :: T2T Blocks
 table = try $ do
   header <- fmap snd <$> option mempty (headerRow)
-  rows <- many1 (many comment *> tableRow)
+  rows <- many1 (many commentLine *> tableRow)
   let columns = transpose rows
   let ncolumns = length columns
   let aligns = map (foldr findAlign AlignDefault) (map (map fst) columns)
@@ -204,7 +185,7 @@ pad :: Monoid a => Int -> [a] -> [a]
 pad n xs = xs ++ (replicate (length xs - n) mempty)
 
 findAlign :: Alignment -> Alignment -> Alignment 
-findAlign AlignDefault x = AlignDefault
+findAlign AlignDefault _ = AlignDefault
 findAlign x AlignDefault = x
 findAlign x y 
   | x == y = x
@@ -226,17 +207,12 @@ tableCell :: T2T (Alignment, Blocks)
 tableCell = try $ do
   leftSpaces <- length <$> many1 space
   content <- (many1Till inline (try $ lookAhead (void newline <|> (many1 space *> endOfCell))))
-  a <- getInput
-  traceShow ("tableCell", a, leftSpaces) (return ())
   rightSpaces <- length <$> many space 
   let align = 
         case compare leftSpaces rightSpaces of
               LT -> AlignLeft
               EQ -> AlignCenter
               GT -> AlignRight
-  trace "here" (return ())
-  a <- getInput
-  traceShow a (return ())
   endOfCell
   return $ (align, B.para (mconcat content))
 
@@ -258,6 +234,7 @@ taggedBlock = do
 
 -- Generic
 
+genericBlock :: Monoid a => T2T a -> (a -> Blocks) -> String -> T2T Blocks
 genericBlock p f s = blockMarkupArea p f s <|> blockMarkupLine p f s 
 
 blockMarkupArea :: Monoid a => (T2T a) -> (a -> Blocks) -> String -> T2T Blocks
@@ -268,83 +245,170 @@ blockMarkupArea p f s = try $ (do
 blockMarkupLine :: T2T a -> (a -> Blocks) -> String -> T2T Blocks
 blockMarkupLine p f s = try (f <$> (string s *> p))
 
+-- Can be in either block or inline position
+comment :: Monoid a => T2T a
+comment = try $ do
+  atStart
+  mempty <$ (char '%' *> anyLine)
+
 -- Inline
+
+parseInlines :: T2T Inlines
+parseInlines = trimInlines . mconcat <$> many1 inline
 
 inline :: T2T Inlines
 inline = do
   choice 
-    [ link
+    [ endline  
+    , commentLine
+    , whitespace
+    , url
+    , link
     , image
     , bold
     , underline
+    , code
+    , raw
+    , tagged
     , strike
     , italic
     , code
     , str
     , symbol
-    , whitespace
     ]
 
 
 bold :: T2T Inlines
-bold = inlineMarkup inline B.strong '*'
+bold = inlineMarkup inline B.strong '*' (B.str)
 
 underline :: T2T Inlines
-underline = inlineMarkup inline B.emph '_'
+underline = inlineMarkup inline B.emph '_' (B.str)
 
 strike :: T2T Inlines
-strike = inlineMarkup inline B.strikeout '-'
+strike = inlineMarkup inline B.strikeout '-' (B.str)
 
 italic :: T2T Inlines
-italic = inlineMarkup inline B.emph '/'
+italic = inlineMarkup inline B.emph '/' (B.str)
 
 code :: T2T Inlines
-code = inlineMarkup ((:[]) <$> anyChar) B.code '`'
+code = inlineMarkup ((:[]) <$> anyChar) B.code '`' id
 
 raw :: T2T Inlines
-raw = inlineMarkup ((:[]) <$> anyChar) B.text '"'
+raw = inlineMarkup ((:[]) <$> anyChar) B.text '"' id
 
 tagged :: T2T Inlines
 tagged = do
   target <- getTarget
-  inlineMarkup ((:[]) <$> anyChar) (B.rawInline target) '\''
+  inlineMarkup ((:[]) <$> anyChar) (B.rawInline target) '\'' id
 
-inlineMarkup :: Monoid a => (T2T a) -> (a -> Inlines) -> Char -> T2T Inlines
-inlineMarkup p f c = try $ do
+-- Parser for markup indicated by a double character.
+-- Inline markup is greedy and glued
+-- Greedy meaning ***a*** = Bold [Str "*a*"]
+-- Glued meaning that markup must be tight to content
+-- Markup can't pass newlines
+inlineMarkup :: Monoid a 
+             => (T2T a) -- Content parser
+             -> (a -> Inlines) -- Constructor
+             -> Char -- Fence
+             -> (String -> a) -- Special Case to handle ******
+             -> T2T Inlines
+inlineMarkup p f c special = try $ do
   start <- many1 (char c)
-  guard (length start >= 2)
-  lookAhead (noneOf [' ', c])
-  rawBody <-  manyTill anyChar (try $ lookAhead (noneOf " " >> string [c,c] ))
-  lastChar <- anyChar 
-  end <- many1 (char c)
-  s <- getState
-  let parser inp = runT2T (mconcat <$> many p) s inp 
-  let start' = either (const mempty) id (parser (drop 2 start))
-  let Right body = parser (rawBody ++ [lastChar])
-  let end' = either (const mempty) id (parser (drop 2 start))
-  return $ f (start' <> body <> end')
+  let l = length start
+  guard (l >= 2)
+  when (l == 2) (void $ notFollowedBy space)
+  -- We must make sure that there is no space before the start of the 
+  -- closing tags
+  body <-  optionMaybe (try $ manyTill (noneOf "\n\r") $ 
+                (try $ lookAhead (noneOf " " >> string [c,c] )))
+  case body of
+    Just middle -> do
+      lastChar <- anyChar 
+      end <- many1 (char c)
+      s <- getState
+      let parser inp = runParser (mconcat <$> many p) s "" inp 
+      let start' = special (drop 2 start)
+      let Right body' = parser (middle ++ [lastChar])
+      let end' = special (drop 2 end)
+      return $ f (start' <> body' <> end')
+    Nothing -> do -- Either bad or case such as *****
+      guard (l >= 5)  
+      let body' = (replicate (l - 4) c)
+      return $ f (special body')
+      
+link :: T2T Inlines
+link = try imageLink <|> titleLink
 
+-- Link with title
 titleLink :: T2T Inlines
 titleLink = try $ do
   char '['
-  tokens <- words <$> manyTill (noneOf " ") (char ']')
+  notFollowedBy space
+  tokens <- sepBy1 (many $ noneOf " ]") space
+  guard (length tokens >= 2)
+  char ']'
   let l = last tokens 
-  let alt = B.text $ concat (intersperse " " (init tokens))
-  return $ B.link (escapeURI l) "" alt
+  guard (length l > 0)
+  let tit = concat (intersperse " " (init tokens))
+  return $ B.link l tit mempty
 
+-- Link with image
 imageLink :: T2T Inlines
 imageLink = try $ do 
   char '['
   body <- image
-  spaces
-  l <- manyTill anyChar (char ']')
+  many1 space
+  l <- manyTill (noneOf "\n\r ") (char ']')
   return (B.link l "" body)
 
-link :: T2T Inlines
-link = imageLink <|> titleLink
+-- raw URLs in text are automatically linked
+url :: T2T Inlines
+url = try $ do
+  (rawUrl, escapedUrl) <- (try uri <|> emailAddress)
+  return $ B.link rawUrl escapedUrl mempty
+  
+uri :: T2T (String, String)
+uri = try $ do
+  address <- t2tURI 
+  return (address, escapeURI address)
+
+-- The definition of a URI in the T2T source differs from the 
+-- actual definition. This is a transcription of the definition in 
+-- the source of v2.6
+--isT2TURI :: String -> Bool
+--isT2TURI (parse t2tURI "" -> Right _) = True
+--isT2TURI _ = False
+
+t2tURI :: T2T String
+t2tURI = do
+  start <- try ((++) <$> proto <*> urlLogin) <|> guess
+  domain <- many1 chars
+  sep <- many (char '/')
+  form' <- option mempty ((:) <$> char '?' <*> many1 form)
+  anchor' <- option mempty ((:) <$> char '#' <*> many anchor)
+  return (start ++ domain ++ sep ++ form' ++ anchor')
+  where 
+    protos = ["http", "https", "ftp", "telnet", "gopher", "wais"]
+    proto = (++) <$> oneOfStrings protos <*> string "://" 
+    guess = (++) <$> (((++) <$> stringAnyCase "www" <*> option mempty ((:[]) <$> oneOf "23")) 
+              <|> stringAnyCase "ftp") <*> ((:[]) <$> char '.')
+    login = alphaNum <|> oneOf "_.-"
+    pass = many (noneOf " @")
+    chars = alphaNum <|> oneOf "%._/~:,=$@&+-"
+    anchor = alphaNum <|> oneOf "%._0"
+    form = chars <|> oneOf ";*"
+    urlLogin = option mempty $ try ((\x y z -> x ++ y ++ [z]) <$> many1 login <*> option mempty ((:) <$> char ':' <*> pass) <*> char '@')
+    
 
 image :: T2T Inlines
-image =  try $ (\url -> B.image url "" mempty) <$> enclosed (char '[') (char ']') (noneOf " ")
+image =  try $ do
+  -- List taken from txt2tags source
+  let extensions = [".jpg", ".jpeg", ".gif", ".png", ".eps", ".bmp"]
+  char '['
+  path <- manyTill (noneOf "\n\t\r ") (try $ lookAhead (oneOfStrings extensions))
+  ext <- oneOfStrings extensions
+  char ']'
+  return $ B.image (path ++ ext) "" mempty 
 
 -- Characters used in markup
 specialChars :: String
@@ -356,18 +420,33 @@ tab = char '\t'
 space :: T2T Char
 space = char ' '
 
+spaces :: T2T String
+spaces = many space
+
+endline :: T2T Inlines
+endline = try $ do
+  newline 
+  notFollowedBy blankline 
+  notFollowedBy hrule
+  notFollowedBy title
+  notFollowedBy commentBlock
+  notFollowedBy verbatim
+  notFollowedBy rawBlock
+  notFollowedBy taggedBlock
+  notFollowedBy quote
+  notFollowedBy list
+  notFollowedBy table
+  return $ B.space
+
 str :: T2T Inlines 
-str = try $ B.str <$> many1 (noneOf $ specialChars ++ "\n\r ")
+str = try $ do
+  B.str <$> many1 (noneOf $ specialChars ++ "\n\r ")
 
 whitespace :: T2T Inlines 
-whitespace = try $ B.space <$ oneOf "\n\r "
+whitespace = try $ B.space <$ spaceChar
 
 symbol :: T2T Inlines
 symbol = B.str . (:[]) <$> oneOf specialChars 
-
--- Fix Up
-
---isURI
 
 -- Utility
 
@@ -378,7 +457,7 @@ getTarget = do
   return target
 
 atStart :: T2T ()
-atStart = (sourceColumn <$> getPosition) >>= guard . (== 0)
+atStart = (sourceColumn <$> getPosition) >>= guard . (== 1)
 
 ignoreSpacesCap :: T2T String -> T2T String
 ignoreSpacesCap p = map toLower <$> (spaces *> p <* spaces)
