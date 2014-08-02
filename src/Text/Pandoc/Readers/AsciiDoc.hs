5aad6c91be75b010dde4a198a9c2548b8ea21bbe
Matthew Pickering <matthewtpickering@gmail.com>
2014-08-02 20:01:59 +0100

    More framework

--- src/Text/Pandoc/Readers/AsciiDoc.hs
+++ src/Text/Pandoc/Readers/AsciiDoc.hs
@@ -65,8 +65,8 @@ delimitedBlock = undeined
 table :: AsciiDoc Table
 table = undefined
 
-attributeList :: A
-
+horizontalRule :: AsciiDoc Blocks
+horizontalRule = undefined
 
 -- Inlines
 
@@ -88,6 +88,56 @@ replacement = undefined
 inlineMacro :: AsciiDoc Inlines
 inlineMacro = undefined
 
+-- Formatting
+emph :: AsciiDoc Inlines
+emph = undefined
+
+strong :: AsciiDoc Inlines
+strong = undefined
+
+mono :: AsciiDoc Inlines
+mono = undefined
+
+quote :: AsciiDoc Inlines
+quote = undefined
+
+doubleQuote :: AsciiDoc Inlines
+doubleQuote = undefined
+
+unquote :: AsciiDoc Inlines
+unquote = undefined
+
+escape :: AsciiDoc Inlines
+escape = undefined
+
+attributes :: AsciiDoc Attr
+attributes = undefined
+
+superscript :: AsciiDoc Inlines
+superscript = undefined
+
+subscript :: AsciiDoc Inlines
+subscript = undefined
+
+linebreak :: AsciiDoc Inlines
+linebreak = undefined
+
+replacement :: AsciiDoc Inlines
+replacement = undefined
+
+specialWords :: AsciiDoc Inlines
+specialWords = undefined
+
+str :: AsciiDoc Inlines
+str = undefined
+
+symbol :: AsciiDoc Inlines
+symbol = undefined
 
 
 -- Utility
+
+specialChars = [Char]
+specialChars = "^*/+-._="
+
+
