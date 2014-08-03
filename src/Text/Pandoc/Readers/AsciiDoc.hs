c877d2d01ac40431d9901c01e53cf5d90db784da
Matthew Pickering <matthewtpickering@gmail.com>
2014-08-03 21:12:31 +0100

    Added inline parsing

--- src/Text/Pandoc/Readers/AsciiDoc.hs
+++ src/Text/Pandoc/Readers/AsciiDoc.hs
@@ -1,22 +1,41 @@
+{-# LANGUAGE RankNTypes, FlexibleInstances, MultiParamTypeClasses #-}
 module Text.Pandoc.Readers.AsciiDoc where
 
+import Text.Pandoc.Definition
+import Text.Pandoc.Options
 import Text.Pandoc.Builder (Inlines, Blocks)
 import qualified Text.Pandoc.Builder as B
-import Text.Pandoc.Parsing
+import Text.Pandoc.Parsing hiding (escaped, many, optional, (<|>))
+import Data.Default (Default, def)
 
+import Control.Monad
+import Control.Monad.Identity
+import Control.Monad.Reader
+import Control.Applicative
+import Data.Char
 
-readAsciiDoc :: ReaderOptions -> String -> Pandoc
-readAsciiDoc = undefined
+import Data.Monoid
 
-data AsciiDocState = AsciiDocState {}
+import Debug.Trace
+
+-- Lenses are defined for these records
+data AsciiDocState = AsciiDocState { aLastStrPosition :: Maybe SourcePos }
+
+data AsciiDocLocal = AsciiDocLocal { aInQuoteContext :: QuoteContext }
 
 instance Default AsciiDocState where
-  def = AsciiDocState
+  def = AsciiDocState Nothing
 
-type AsciiDoc = ParserT String AsciiDocState Identity
+instance Default AsciiDocLocal where
+  def = AsciiDocLocal NoQuote
+
+type AsciiDoc = ParserT String AsciiDocState (Reader AsciiDocLocal)
 
 readAsciiDoc :: ReaderOptions -> String -> Pandoc
-readAsciiDoc = readWith parseAsciiDoc def (s ++ "\n\n")
+readAsciiDoc os s = runAsciiDoc parseAsciiDoc def s
+
+runAsciiDoc :: AsciiDoc a -> AsciiDocState -> String -> a
+runAsciiDoc asdoc st s =  flip runReader def $ readWithM asdoc st (s ++ "\n\n")
 
 parseAsciiDoc :: AsciiDoc Pandoc
 parseAsciiDoc = do
@@ -32,6 +51,7 @@ header = do
   author
   revision
   blankline
+  return undefined
 
 documentTitle :: AsciiDoc ()
 documentTitle = undefined
@@ -69,13 +89,13 @@ list :: AsciiDoc Blocks
 list = undefined
 
 delimitedBlock :: AsciiDoc Blocks
-delimitedBlock = undeined
+delimitedBlock = undefined
 
-table :: AsciiDoc Table
+table :: AsciiDoc Blocks
 table = undefined
 
 horizontalRule :: AsciiDoc Blocks
-horizontalRule = undefined
+horizontalRule = atStart *> string "'''" *> many (char '\'') *> blankline *> return B.horizontalRule
 
 -- Inlines
 
@@ -83,82 +103,136 @@ parseInlines :: AsciiDoc Inlines
 parseInlines = undefined
 
 inline :: AsciiDoc Inlines
-inline = undefined
+inline = choice
+          [ replacement
+          , escaped
+          , endline
+          , emph
+          , strong
+          , mono
+          , quote
+          , doubleQuote
+          , unquote
+          , linebreak ]
 
 specialCharacter :: AsciiDoc Inlines
 specialCharacter = undefined
 
-quote :: AsciiDoc Inlines
-quote = undefined
-
 specialWord :: AsciiDoc Inlines
 specialWord = undefined
-
-replacement :: AsciiDoc Inlines
-replacement = undefined
 
 inlineMacro :: AsciiDoc Inlines
 inlineMacro = undefined
 
 -- Formatting
 emph :: AsciiDoc Inlines
-emph = quotedText '_' B.emph
+emph = quotedText '_' inline B.emph
 
 strong :: AsciiDoc Inlines
-strong = quotedText '*' B.strong
+strong = quotedText '*' inline B.strong
 
 mono :: AsciiDoc Inlines
-mono = quotedText '+' B.code
+mono = quotedText '+' ((:[]) <$> anyChar) B.code
 
 quote :: AsciiDoc Inlines
-quote = undefined
+quote = quoted inline
+
+
 
 doubleQuote :: AsciiDoc Inlines
 doubleQuote = undefined
 
 unquote :: AsciiDoc Inlines
-unquote = quotedText '#' id
+unquote = quotedText '#' inline id
+
+
+quotedText :: Monoid a => Char -> AsciiDoc a -> (a -> Inlines) -> AsciiDoc Inlines
+quotedText c p f = consText c p f  <|> unconsText [c, c] p f
 
 -- Constrained as defined here-
 -- https://groups.google.com/forum/#!searchin/asciidoc/constrained/asciidoc/-jgLVHILFRg/3K-K4gXoFosJ
 -- Surrounded by any character in the set [^a-zA-Z0-9_]
-quotedText :: Char -> (Inlines -> Inlines) -> AsciiDoc Inlines
-quotedText c f = try $ do
+consText :: Monoid a => Char -> AsciiDoc a -> (a -> Inlines) -> AsciiDoc Inlines
+consText c p f = try $ do
   let boundary x = not (isAlphaNum x || x == '_')
-  b <- B.text . (:[]) <$> (satisfy boundary) <|> (atStart *> return mempty)
-  r <- enclosed (char c) (char c) (inline)
+  b <- (atStart *> return mempty) <|> (mempty <$ lookAhead attributeList) <|> (B.text . (:[]) <$> satisfy boundary)
+  cons <- maybe id B.spanWith <$> optionMaybe attributeList
+  r <- mconcat <$> enclosed (char c) (char c) p
   lookAhead (satisfy boundary)
-  return (b <> f r)
+  return (b <> cons (f r))
 
-escape :: AsciiDoc Inlines
-escape = undefined
+unconsText :: Monoid a => String -> AsciiDoc a -> (a -> Inlines) -> AsciiDoc Inlines
+unconsText bound p f = try $ do
+  f <$> mconcat <$> enclosed (string bound) (string bound) p
+
+
+attributeList :: AsciiDoc Attr
+attributeList = do
+  char '['
+  classes <- manyTill (posAttr <* spaces <* char ',' <* spaces) (lookAhead namedAttr)
+  spaces
+  optional $ char ','
+  spaces
+  kvs <- sepBy namedAttr (char ',')
+  char ']'
+  return ("", classes, kvs)
+  where
+    posAttr = try $ do
+      notFollowedBy namedAttr
+      many1 (noneOf ",]")
+    namedAttr = try $ (,) <$> many1 (noneOf "=,") <* char '=' <*> value
+    value = try (many1 (noneOf "\",]")) <|> (concat <$> enclosed (char '"') (char '"') (string "\\\"" <|> (:[]) <$> noneOf "\"" ))
+
+escaped :: AsciiDoc Inlines
+escaped = try $ B.str . (:[]) <$> (char '\\' *> oneOf specialChars)
 
 attributes :: AsciiDoc Attr
 attributes = undefined
 
 superscript :: AsciiDoc Inlines
-superscript = undefined
+superscript = unconsText "^" inline B.superscript
 
 subscript :: AsciiDoc Inlines
-subscript = undefined
+subscript = unconsText "~" inline B.subscript
 
 linebreak :: AsciiDoc Inlines
-linebreak = undefined
+linebreak = B.linebreak <$ try (space *> char '+')
 
 replacement :: AsciiDoc Inlines
-replacement = undefined
+replacement = choice (map (uncurry replace) replacements)
+  where
+    replace s alt = try $ string s *> return (B.str alt)
 
+
+replacements :: [(String, String)]
+replacements = [ ("(C)", "\x00A9")
+               , ("(TM)", "\x2122")
+               , ("(R)",  "\x00AE")
+               , ("--", "\x2013")
+               , ("---", "\x2014")
+               , ("->", "\x2192")
+               , ("<-", "\x2190")
+               , ("=>", "\x21D2")
+               , ("<=", "\x21D0")]
+
+-- Unimplemented until config reading is added
 specialWords :: AsciiDoc Inlines
 specialWords = undefined
 
-space :: AsciiDoc Inlines
-space = undefined
+spacec :: AsciiDoc Inlines
+spacec = undefined
+
+endline :: AsciiDoc Inlines
+endline = undefined
 
 str :: AsciiDoc Inlines
-str = undefined
+str = B.str <$> many1 (noneOf (specialChars ++ " \n\r\t"))
+
+whitespace :: AsciiDoc Inlines
+whitespace = B.space <$ oneOf " \n\r\t"
 
 symbol :: AsciiDoc Inlines
-symbol = undefined
+symbol = B.str . (:[]) <$> oneOf specialChars
 
 
 -- Utility
@@ -166,7 +240,40 @@ symbol = undefined
 atStart :: AsciiDoc ()
 atStart = getPosition >>= guard . (== 1) . sourceColumn
 
-specialChars = [Char]
-specialChars = "^*/+-._="
+specialChars :: [Char]
+specialChars = "^*/+-._='#"
 
+-- Lenses
+
+type Lens s a = Functor f => (a -> f a) -> s -> f s
+
+over :: Lens s a -> (a -> a) -> s -> s
+over ln f s = runIdentity $ ln (Identity . f) s
+
+view :: Lens s a -> s -> a
+view ln s = getConst $ ln Const s
+
+set :: Lens s a -> a -> s -> s
+set ln x = over ln (const x)
+
+lastStrPosition :: Lens AsciiDocState (Maybe SourcePos)
+lastStrPosition f asciistate  =
+  fmap (\newPos -> asciistate { aLastStrPosition = newPos })
+    (f (aLastStrPosition asciistate))
+
+
+inQuoteContext :: Lens AsciiDocLocal QuoteContext
+inQuoteContext f asciistate  =
+  fmap (\newpos -> asciistate { aInQuoteContext = newpos })
+    (f (aInQuoteContext asciistate))
+
+-- Instances
+
+instance HasLastStrPosition AsciiDocState where
+  setLastStrPos = set lastStrPosition . Just
+  getLastStrPos = view lastStrPosition
+
+instance HasQuoteContext st (Reader AsciiDocLocal) where
+  getQuoteContext = asks (view inQuoteContext)
+  withQuoteContext c = local (set inQuoteContext c)
 
