fc8adbfe691fb97ba6d109a3bec384aeaf149ee4
Matthew Pickering <matthewtpickering@gmail.com>
2014-08-05 00:02:50 +0100

    Added delimited blocks

--- src/Text/Pandoc/Readers/AsciiDoc.hs
+++ src/Text/Pandoc/Readers/AsciiDoc.hs
@@ -3,7 +3,7 @@ module Text.Pandoc.Readers.AsciiDoc where
 
 import Text.Pandoc.Definition
 import Text.Pandoc.Options
-import Text.Pandoc.Builder (Inlines, Blocks)
+import Text.Pandoc.Builder (Inlines, Blocks, trimInlines)
 import qualified Text.Pandoc.Builder as B
 import Text.Pandoc.Parsing hiding (escaped, many, optional, (<|>))
 import Data.Default (Default, def)
@@ -14,28 +14,31 @@ import Control.Monad.Reader
 import Control.Applicative
 import Data.Char
 
+import Data.List
 import Data.Monoid
+import Data.Maybe (fromMaybe)
 
 import Debug.Trace
 
 -- Lenses are defined for these records
 data AsciiDocState = AsciiDocState { aLastStrPosition :: Maybe SourcePos }
 
-data AsciiDocLocal = AsciiDocLocal { aInQuoteContext :: QuoteContext }
+data AsciiDocLocal = AsciiDocLocal { aInQuoteContext :: QuoteContext
+                                  ,  aInVerse :: Bool }
 
 instance Default AsciiDocState where
   def = AsciiDocState Nothing
 
 instance Default AsciiDocLocal where
-  def = AsciiDocLocal NoQuote
+  def = AsciiDocLocal NoQuote False
 
 type AsciiDoc = ParserT String AsciiDocState (Reader AsciiDocLocal)
 
 readAsciiDoc :: ReaderOptions -> String -> Pandoc
-readAsciiDoc os s = runAsciiDoc parseAsciiDoc def s
+readAsciiDoc os s = runAsciiDoc parseAsciiDoc def (s ++ "\n\n")
 
 runAsciiDoc :: AsciiDoc a -> AsciiDocState -> String -> a
-runAsciiDoc asdoc st s =  flip runReader def $ readWithM asdoc st (s ++ "\n\n")
+runAsciiDoc asdoc st s =  flip runReader def $ readWithM asdoc st s
 
 parseAsciiDoc :: AsciiDoc Pandoc
 parseAsciiDoc = do
@@ -76,33 +79,147 @@ attributeEntry = undefined
 parseBlocks :: AsciiDoc Blocks
 parseBlocks = undefined
 
+appendAttr :: Attr -> Attr -> Attr
+appendAttr (id1, cs, kvs) (_, cs', kvs') = (id1, cs ++ cs', kvs ++ kvs')
+
 block :: AsciiDoc Blocks
-block = undefined
+block = do
+  as@(_, cs, _) <- foldr appendAttr nullAttr <$> many (attributeList <* blankline)
+  let cons = if as == nullAttr then id else B.divWith as
+  traceShow (cons mempty) (return ())
+  b <- choice
+        [ title
+        , literal
+        , admonition
+        , delimitedBlocks
+        , local (set inVerse ("verse" `elem` cs)) para
+        ]
+  return $ cons b
 
 title :: AsciiDoc Blocks
 title = twoLineTitle <|> oneLineTitle
 
 twoLineTitle :: AsciiDoc Blocks
 twoLineTitle = try $ do
-  tit <- anyLine
+  text <- anyLine
   under <- lookAhead anyChar
   guard (under `elem` "=-~&+")
   len <- length <$> many1 (char under)
-  let titLen = length title
-  guard (len >= titLen - 2 && len <= titlen + 2)
+  let titLen = length text
+  guard (len >= titLen - 2 && len <= titLen + 2)
+  let level = fromMaybe (error "Invalid header underline") (findIndex (== under) headerChars)
+  titleText <- parseFromString parseInlines text
+  return $ B.header level titleText
   where
-    headers = "=-~&+"
+    headerChars = "=-~&+"
 
+oneLineTitle :: AsciiDoc Blocks
+oneLineTitle = try $ do
+  level <- (\x -> length x - 1) <$> many1 (char '=')
+  guard (level <= 4)
+  space
+  let titleEnd = many (char '=') *> blankline
+  titleText <- trimInlines . mconcat <$> many1 (notFollowedBy titleEnd *> inline)
+  return $ B.header level titleText
+
+blockId :: AsciiDoc Blocks
+blockId = try $ do
+  divId <- enclosed (string "[[") (string "]]") (noneOf "]")
+  blankline
+  return $ B.divWith (divId, [], []) mempty
+
+para :: AsciiDoc Blocks
+para = try $ do
+  ils <- parseInlines
+  nl <- option False (True <$ newline)
+  option (B.plain ils) (guard nl >> notFollowedBy listStart >> return (B.para ils))
+  where
+    listStart = char '*'
+
+admonition :: AsciiDoc Blocks
+admonition = try $ do
+    choice (map mkAdmonParser admonitions)
+  where
+    mkAdmonParser s = try $ string s *> char ':' *> spaces *> (B.divWith ("", [s], []) <$> para)
+
+admonitions :: [String]
+admonitions = ["NOTE", "CAUTION", "TIP", "IMPORTANT", "WARNING"]
+
+literal :: AsciiDoc Blocks
+literal = try $ do
+  ind <- length <$> lookAhead (many1 spaceChar)
+  B.codeBlock . unlines <$> many1 (count ind spaceChar *> anyLine)
+
+delimitedBlocks :: AsciiDoc Blocks
+delimitedBlocks =
+  lookAhead (count 4 (oneOf dchars)) *>  -- Not sure if this optimisation is worthwhile
+  choice
+    [ commentBlock
+    , passthroughBlock
+    , listingBlock
+    , literalBlock
+    , sidebarBlock
+    , quoteBlock
+    , exampleBlock ]
+  where
+    dchars = "/+-._=*"
+
+commentBlock :: AsciiDoc Blocks
+commentBlock = delimitedBlock '/' anyLine (const mempty)
+
+passthroughBlock :: AsciiDoc Blocks
+passthroughBlock = delimitedBlock '+' inline B.para
+
+listingBlock :: AsciiDoc Blocks
+listingBlock = delimitedBlock '-' anyLine B.codeBlock
+
+literalBlock :: AsciiDoc Blocks
+literalBlock = delimitedBlock '.' anyLine B.codeBlock
+
+sidebarBlock :: AsciiDoc Blocks
+sidebarBlock = delimitedBlock '*' block (B.para . B.note)
+
+quoteBlock :: AsciiDoc Blocks
+quoteBlock = delimitedBlock '_' inline ((B.divWith ("", ["quote"], [])) . B.para)
+
+exampleBlock :: AsciiDoc Blocks
+exampleBlock = delimitedBlock '=' inline ((B.divWith ("", ["example"], [])) . B.para)
+
+delimitedBlock :: Monoid a => Char -> (AsciiDoc a) -> (a -> Blocks) -> AsciiDoc Blocks
+delimitedBlock fence parser constructor = try $ do
+  count 4 (char fence) *> many (char fence) *> blankline
+  r <- mconcat <$> manyTill parser (count 4 (char fence) *> many (char fence) *> blankline)
+  return (constructor r)
+
+openBlock :: AsciiDoc Blocks
+openBlock = try $ do
+  string "--" *> blankline
+  parseBlocks *> string "--" *> blankline
 
 
 blockMacros :: AsciiDoc Blocks
 blockMacros = undefined
 
-list :: AsciiDoc Blocks
-list = undefined
+-- Lists
 
-delimitedBlock :: AsciiDoc Blocks
-delimitedBlock = undefined
+list :: AsciiDoc Blocks
+list = bulletList <|> numberedList <|> labeledList <|> calloutList
+
+bulletList :: AsciiDoc Blocks
+bulletList = B.bulletList . compactify'
+             <$> many1 (listItem bulletListStart parseBlocks)
+
+
+numberedList :: AsciiDoc Blocks
+numberedList = undefined
+
+labeledList :: AsciiDoc Blocks
+labeledList = undefined
+
+calloutList :: AsciiDoc Blocks
+calloutList = undefined
+
+-- Table
 
 table :: AsciiDoc Blocks
 table = undefined
@@ -113,20 +230,20 @@ horizontalRule = atStart *> string "'''" *> many (char '\'') *> blankline *> ret
 -- Inlines
 
 parseInlines :: AsciiDoc Inlines
-parseInlines = undefined
+parseInlines = trimInlines . mconcat <$> many1 inline
 
 inline :: AsciiDoc Inlines
 inline = choice
           [ replacement
           , escaped
-          , endline
           , emph
           , strong
           , mono
           , quote
-          , doubleQuote
           , unquote
-          , linebreak ]
+          , str
+          , linebreak
+          , whitespace ]
 
 specialCharacter :: AsciiDoc Inlines
 specialCharacter = undefined
@@ -149,11 +266,6 @@ mono = quotedText '+' ((:[]) <$> anyChar) B.code
 
 quote :: AsciiDoc Inlines
 quote = quoted inline
-
-
-
-doubleQuote :: AsciiDoc Inlines
-doubleQuote = undefined
 
 unquote :: AsciiDoc Inlines
 unquote = quotedText '#' inline id
@@ -180,27 +292,24 @@ unconsText bound p f = try $ do
 
 
 attributeList :: AsciiDoc Attr
-attributeList = do
+attributeList = try $ do
   char '['
-  classes <- manyTill (posAttr <* spaces <* char ',' <* spaces) (lookAhead namedAttr)
-  spaces
-  optional $ char ','
-  spaces
+  let sep = spaces *> optional (char ',') *> spaces
+  classes <- manyTill (posAttr <* sep) (lookAhead (void (try (lookAhead namedAttr)) <|> void ((char ']'))))
+  traceShow "here" (return ())
   kvs <- sepBy namedAttr (char ',')
+  traceShow "fail" (return ())
+  a <- getInput
+  traceShow (a, classes, kvs) (return ())
   char ']'
   return ("", classes, kvs)
   where
-    posAttr = try $ do
-      notFollowedBy namedAttr
-      many1 (noneOf ",]")
-    namedAttr = try $ (,) <$> many1 (noneOf "=,") <* char '=' <*> value
+    posAttr = try $ many1 (noneOf ",]")
+    namedAttr = try $ (,) <$> many1 (noneOf "]=,") <* char '=' <*> value
     value = try (many1 (noneOf "\",]")) <|> (concat <$> enclosed (char '"') (char '"') (string "\\\"" <|> (:[]) <$> noneOf "\"" ))
 
 escaped :: AsciiDoc Inlines
 escaped = try $ B.str . (:[]) <$> (char '\\' *> oneOf specialChars)
-
-attributes :: AsciiDoc Attr
-attributes = undefined
 
 superscript :: AsciiDoc Inlines
 superscript = unconsText "^" inline B.superscript
@@ -271,15 +380,18 @@ set ln x = over ln (const x)
 
 lastStrPosition :: Lens AsciiDocState (Maybe SourcePos)
 lastStrPosition f asciistate  =
-  fmap (\newPos -> asciistate { aLastStrPosition = newPos })
+  fmap (\newpos -> asciistate { aLastStrPosition = newpos })
     (f (aLastStrPosition asciistate))
-
 
 inQuoteContext :: Lens AsciiDocLocal QuoteContext
 inQuoteContext f asciistate  =
   fmap (\newpos -> asciistate { aInQuoteContext = newpos })
     (f (aInQuoteContext asciistate))
 
+inVerse :: Lens AsciiDocLocal Bool
+inVerse f asciistate  =
+  fmap (\newpos -> asciistate { aInVerse = newpos })
+    (f (aInVerse asciistate))
 -- Instances
 
 instance HasLastStrPosition AsciiDocState where
